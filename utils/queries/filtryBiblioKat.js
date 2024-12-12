980499dc06abd9f6641d9aecd17b0fc069c57d33
nkocjan <kocjan.nikodem@gmail.com>
2024-12-12 09:18:49 +0100

    fix utils

--- utils/queries/filtryBiblioKat.js
+++ utils/queries/filtryBiblioKat.js
@@ -1,19 +1,20 @@
-const pool = require('../../db');
+const pool = require("../../db");
 
 async function getBibliotekiAndKategorie() {
   try {
-    const bibliotekiResult = await pool.query("SELECT * FROM project.biblioteka");
+    const bibliotekiResult = await pool.query(
+      "SELECT * FROM project.biblioteka"
+    );
     const kategorieResult = await pool.query("SELECT * FROM project.kategoria");
 
     const kategorie = kategorieResult.rows;
     const biblioteki = bibliotekiResult.rows;
 
     return { kategorie, biblioteki };
-  } catch(err) {
+  } catch (err) {
     console.log("KRYTYCZNY ERROR", err);
     return { kategorie: [], biblioteki: [] };
   }
 }
-
 
 module.exports = getBibliotekiAndKategorie;
--- utils/queries/generateBookQuery.js
+++ utils/queries/generateBookQuery.js
@@ -4,36 +4,44 @@ function generateBookQuery(filters) {
   let conditions = [];
 
   if (filters.filtr_tytul) {
-      conditions.push(`tytul LIKE $${params.length + 1}`);
-      params.push(`%${filters.filtr_tytul}%`);
+    conditions.push(`tytul LIKE $${params.length + 1}`);
+    params.push(`%${filters.filtr_tytul}%`);
   }
 
   if (filters.filtr_autor) {
-      conditions.push(`autor LIKE $${params.length + 1}`);
-      params.push(`%${filters.filtr_autor}%`);
+    conditions.push(`autor LIKE $${params.length + 1}`);
+    params.push(`%${filters.filtr_autor}%`);
   }
 
   if (filters.kategorie) {
-    const kategorieArray = Array.isArray(filters.kategorie) ? filters.kategorie : [filters.kategorie];
+    const kategorieArray = Array.isArray(filters.kategorie)
+      ? filters.kategorie
+      : [filters.kategorie];
 
-    const categoryPlaceholders = kategorieArray.map((_, i) => `$${params.length + i + 1}`).join(", ");
+    const categoryPlaceholders = kategorieArray
+      .map((_, i) => `$${params.length + i + 1}`)
+      .join(", ");
     conditions.push(`kategoria IN (${categoryPlaceholders})`);
     params = params.concat(kategorieArray);
   }
 
   if (filters.biblioteki) {
-    const bibliotekiArray = Array.isArray(filters.biblioteki) ? filters.biblioteki : [filters.biblioteki];
-    const libraryPlaceholders = bibliotekiArray.map((_, i) => `$${params.length + i + 1}`).join(", ");
+    const bibliotekiArray = Array.isArray(filters.biblioteki)
+      ? filters.biblioteki
+      : [filters.biblioteki];
+    const libraryPlaceholders = bibliotekiArray
+      .map((_, i) => `$${params.length + i + 1}`)
+      .join(", ");
     conditions.push(`biblioteka IN (${libraryPlaceholders})`);
     params = params.concat(bibliotekiArray);
   }
 
-  if (filters.dostepne === 'tak') {
-      conditions.push("dostepne > 0");
+  if (filters.dostepne === "tak") {
+    conditions.push("dostepne > 0");
   }
 
   if (conditions.length > 0) {
-      query += " WHERE " + conditions.join(" AND ");
+    query += " WHERE " + conditions.join(" AND ");
   }
 
   query += ` ORDER BY ${filters.sortowanie}`;
